f5f2b175909a012b7af4cd42932576af7e1c1820
thephotogenicbug <navinkumarachari@gmail.com>
2021-12-29 16:03:02 +0530

    React-Ecommerce-frontend : initial react app with routing and components
    Signed-off-by: thephotogenicbug <navinkumarachari@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/signup" element={<Signup/>} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
