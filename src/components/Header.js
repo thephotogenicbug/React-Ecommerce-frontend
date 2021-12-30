ecc8c71d45db789b0fc5f6465a674fc2ce8eaa21
thephotogenicbug <navinkumarachari@gmail.com>
2021-12-30 18:10:44 +0530

    Change Navbar links using isAuthenticated (doesnt work  withRouter is deprecated)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "./helpers/auth";
 
 const Header = () => {
-
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand">
-            Logo
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link to="/signup" className="nav-link" aria-current="page">
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/signin" className="nav-link">
-                  Signin
-                </Link>
-              </li>
-            </ul>
-          </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Logo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+            {isAuthenticated() && (
+              <>
+                <li className="nav-item">
+                  <Link to="/signup" className="nav-link" aria-current="page">
+                    Signup
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signin" className="nav-link" aria-current="page">
+                    Signin
+                  </Link>
+                </li>
+              </>
+            )}
+            {isAuthenticated() && isAuthenticated.role === 0 && (
+              <>
+                <li className="nav-item">
+                  <Link
+                    to="/user/dashboard"
+                    className="nav-link"
+                    aria-current="page"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+              </>
+            )}
+            {isAuthenticated() && isAuthenticated.role === 1 && (
+              <>
+                <li className="nav-item">
+                  <Link
+                    to="/admin/dashboard"
+                    className="nav-link"
+                    aria-current="page"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+              </>
+            )}
+            {isAuthenticated() &&  (
+              <>
+                <li className="nav-item">
+                  <Link
+                    to="/admin/dashboard"
+                    className="nav-link"
+                    aria-current="page"
+                  >
+                    Logout
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-      </nav>
-    );
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
