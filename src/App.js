24a30b4e57aa17d19f1aeece91883d8dc9f0fed7
thephotogenicbug <navinkumarachari@gmail.com>
2021-12-30 14:28:08 +0530

    Import App.css into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import "./App.css";
 import Header from './components/Header';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+
 
 function App() {
   return (
