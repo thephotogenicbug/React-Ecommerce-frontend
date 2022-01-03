3c316ee8d98d6032a62c25eb19b107608ebeff7a
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-03 15:42:01 +0530

    Add Protected routes using useEffect and getCookies

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { isAuthenticated } from "./helpers/auth";
+import { getCookie } from "./helpers/cookies";
 
-const AdminDashboard = () => {
-    return (
-        <div>
-            <h1>AdminDashboard</h1>
-        </div>
-    )
-}
+const AdminDashboard = ({key}) => {
+  let navigate = useNavigate();
 
-export default AdminDashboard
+  useEffect(() => {
+    if (!getCookie('token')) {
+      navigate("/");
+    }
+  }, [navigate]);
+  return (
+    <div>
+      <h1>AdminDashboard</h1>
+    </div>
+  );
+};
+
+export default AdminDashboard;
--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { getCookie } from './helpers/cookies';
 
 const UserDashboard = () => {
+      let navigate = useNavigate();
+
+      useEffect(() => {
+        if (!getCookie("token")) {
+          navigate("/");
+        }
+      }, [navigate]);
     return (
         <div>
             <h1>UserDashboard</h1>
